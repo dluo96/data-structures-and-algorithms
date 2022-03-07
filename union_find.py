59533bd30298b3da0fb9d0db67837e1502435186
dluo96 <dan.luo1996@gmail.com>
2022-03-07 09:04:11 +0000

    Initial implementation of union-find DS

---
+++ union_find.py
@@ -0,0 +1,69 @@
+class UnionFind():
+    def __init__(self, size) -> None:
+        self.size = size      # The number of elements in this union-find DS
+        self.sz = []       # Used to track the size of each group
+        self.id = []       # id[i] points to the parent of i. If id[i] = i, then i is a root node
+        self.numGroups = 0 # Number of groups in the union-find DS
+
+        if self.size <= 0:
+            raise ValueError("Size <= 0 is not allowed")
+
+        self.numGroups = size # Initially the number of groups is the number of elements
+        self.sz = [None] * self.size
+        self.id = [None] * self.size
+
+        for i in range(0, self.size):
+            self.id[i] = i # Link to itself (self-root)
+            self.sz[i] = 1 # Each group is originally of size 1
+            i += 1
+        
+    
+    def find(self, p):
+        """Find which group the element p belongs to. Takes amortized constant time."""
+
+        # Find the root of the group
+        root = p
+        while root != self.id[root]:
+            root = self.id[root]
+        
+        # Path compression: compress the path leading back to the root
+        # This results in amortized constant time complexity
+        # We do it iteratively (but could also have done it recursively)
+        while p != root:
+            next = self.id[p]
+            self.id[p] = root
+            p = next
+        
+        return root
+    
+
+    def connected(self, p : int, q : int):
+        """Return whether the elements p and q are in the same group"""
+        return self.find(p) == self.find(q)
+    
+
+    def groupSize(self, p : int):
+        """Return the size of the group that p belongs to"""
+        return self.sz[self.find(p)]
+    
+
+    def unify(self, p : int, q : int):
+        """Merge the group containing the element p with the group containing q"""
+        root_p = self.find(p)
+        root_q = self.find(q)
+
+        if root_p == root_q:
+            print(f"Elements {p} and {q} already belong to the same group")
+            return
+
+        # Merge two groups (merge the smaller group into the larger group)
+        if self.sz[root_p] < self.sz[root_q]:
+            self.sz[root_q] += self.sz[root_p]
+            self.id[root_p] = root_q
+        else:
+            self.sz[root_p] += self.sz[root_q]
+            self.id[root_q] = root_p
+        
+        # Since the root nodes were different, we know
+        # that the number of groups has decreased by 1
+        self.numGroups -= 1
